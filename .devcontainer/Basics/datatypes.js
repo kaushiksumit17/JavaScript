c1a0fc5de65bb6a867d99a2ac41ce41abcbe4aba
kaushiksumit17 <116892198+kaushiksumit17@users.noreply.github.com>
2024-02-10 18:47:16 +0000

    datatypes

---
+++ .devcontainer/Basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as new version
+
+// alert(3 + 3) // we are use nodeJs not browser
+
+console.log( 3 
+    + 3); // code readabilty should be high
+
+console.log("sumit");
+
+let name="sumit"
+let age=18
+let isLoggedIn = false
+
+//number => 2^53 ( 2 to power 53)
+// Bigint
+// String => " "
+// boolean => true/false
+// null => stanalone value
+//undefine => 
+// symbol => unique
+
+//object
+
+console.log(typeof "sumit");
+console.log(typeof age);
+console.log(typeof undefined);// output -> Undefined
+console.log(typeof null);// is will print object in output 
---
+++ .devcontainer/Basics/variables.js
@@ -0,0 +1,17 @@
+const accountId= 1445566
+let accountEmail= "sumit@google.com"
+var accountPassword= "123456"
+accountCity= "jaipur"
+
+//accountId=2; not allow in const //
+
+accountEmail="sk@sk.com"
+accountPassword="123443"
+accountCity="Bengaluru"
+let accountState=
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/* prefer not to use var
+    because of issue in block scope and function scope
+*/ 
