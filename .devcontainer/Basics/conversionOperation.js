96ac9ebc30671b66e3967b2b2fdb47af825088c2
kaushiksumit17 <116892198+kaushiksumit17@users.noreply.github.com>
2024-02-10 21:20:07 +0000

    Conversion oF Data Types

---
+++ .devcontainer/Basics/conversionOperation.js
@@ -0,0 +1,25 @@
+let score = undefined
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log( typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//True => 1; False=>0
+
+let isLoggedIn = "sumit"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => True; 0 => False;
+// "" => False
+// "sumit" => True
+
+let someNumber = 33
+let stringNumber = String (someNumber)
+ console.log(stringNumber);
+ console.log(typeof(stringNumber));
