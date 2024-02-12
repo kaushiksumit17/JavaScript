e229c96da8cbd17022d9d8398590adce52c0a3c5
kaushiksumit17 <116892198+kaushiksumit17@users.noreply.github.com>
2024-02-12 04:35:27 +0000

    datatypes

---
+++ .devcontainer/Basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// primitve  datatype
+
+// 7 types: string , number, boolean, null, undefine, symbol, bigint.
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 84713479013840138409234n
+ 
+// reference (Non primitive)
+// Arary, Object, Functions.
+
+const heros =["shaktiman","naagraj","Dooga"]
+
+let myobj{
+    name:"sumit",
+    age:23,
+}
+
+const myfunction=function(){
+    console.log("hello world");
+}
+
+console.log(typeof anotherId);
